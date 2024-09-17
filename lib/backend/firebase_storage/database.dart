5d43fdaef3271b3f7a2b28eb9c7d29c411e9cf49
Nick Gaudet <ngaudet@csus.edu>
2024-09-17 12:20:00 -0700

    Added user document creation on account setup

--- lib/backend/firebase_storage/database.dart
+++ lib/backend/firebase_storage/database.dart
@@ -1,18 +1,35 @@
 import "package:cloud_firestore/cloud_firestore.dart";
+import 'package:firebase_auth/firebase_auth.dart';
 import "package:sqflite/sqflite.dart";
 
-class DatabaseService{
-
+class DatabaseService {
   DatabaseService();
-  final CollectionReference exerciseCollection = FirebaseFirestore.instance.collection("exercises");
+  final CollectionReference exerciseCollection =
+      FirebaseFirestore.instance.collection("exercises");
+  final usersCollection = FirebaseFirestore.instance.collection("users");
 
-  Future createExercise(String name, int numSets, int numReps, String description, String link) async{
+  Future createExercise(String name, int numSets, int numReps,
+      String description, String link) async {
     return await exerciseCollection.doc(name).set({
-      "name" : name,
-      "numSets" : numSets,
-      "numReps" : numReps,
-      "description" : description,
-      "videoLink" : link
+      "name": name,
+      "numSets": numSets,
+      "numReps": numReps,
+      "description": description,
+      "videoLink": link
+    });
+  }
+
+  Future createUser(String name, String username) async {
+    final User? user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      throw Exception("Cannot create user document: currentUser is null.");
+    }
+
+    final uid = user.uid;
+    await usersCollection.doc(uid).set({
+      "name": name,
+      "username": username,
+      "createdAt": FieldValue.serverTimestamp()
     });
   }
 }
